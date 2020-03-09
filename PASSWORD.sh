158853e071e3a38b6b3e687d97b8e0e525a18675
RAJABING <59326452+RAJABING@users.noreply.github.com>
2020-03-09 17:02:10 +0700

    Add files via upload

---
+++ PASSWORD.sh
@@ -0,0 +1,14 @@
+clear
+echo $red "============================================================"
+echo "1.)INI PW TOOLS BINGKU TUAN:BING"
+echo "0.)KELUAR"
+read -p "SILAHKAN KELUAR : " BING
+
+if [ $BING = 0 ] || [ $BING = 0 ]
+then 
+clear
+ls
+ls BINGTOBINGKU.sh
+cd BINGTOBINGKU.sh
+ls BINGTOBINGKU.sh
+sh BINGTOBINGKU.sh 
